d5ccdfdd56f4f8c57284c38214b3a3bf0d00197d
elonwoo <elonwoo@users.noreply.github.com>
2024-09-11 03:42:19 +0800

    fix: add RAYCAST_API_URL in scrape.yml

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 
 import os
 
+RAYCAST_API_URL = os.environ.get('RAYCAST_API_URL')
 APP_ID = os.environ.get('FEISHU_APP_ID')
 APP_SECRET = os.environ.get('FEISHU_APP_SECRET')
 FEISHU_APP_TOKEN = os.environ.get('FEISHU_APP_TOKEN')
 FEISHU_TABLE_ID = os.environ.get('FEISHU_TABLE_ID')
-RAYCAST_API_URL = os.environ.get('RAYCAST_API_URL')
